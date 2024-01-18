2f13d2d04d68869cb28f60e36d096064c478332d
maurice diong <mdiong00@gmail.com>
2024-01-18 12:55:18 -0500

    ok... the last one was a bit tricky. review re module

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+  
+  def __init__(self,value=''):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print('The value must be a string.')
+
+  def is_sentence(self):
+    return True if self.value.endswith('.') else False
+  
+  def is_question(self):
+    return True if self.value.endswith('?') else False
+
+  def is_exclamation(self):
+    return True if self.value.endswith('!') else False 
+  
+  def count_sentences(self):
+    sentences = re.split(r'[?.!]+',self.value)
+    # breakpoint()
+    # breakpoint()
+    sentences = [sentence for sentence in sentences if sentence != ""]
+    # breakpoint()
+    return len(sentences)
